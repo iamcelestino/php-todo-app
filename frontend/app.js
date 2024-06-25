928e58e13c3cc1af360b467a85d84b72d18814e3
iamcelestino <trcelestino488@gmail.com>
2024-06-25 01:52:26 -0700

    feat: render todos in the frontEnd

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,38 @@
 
-function getTasks() {
-    fetch('http://127.0.0.1/Todo-php-app/backend/apis/list-all.php')
-        .then(response => response.json())
-        .then(tasks => {
-            console.log(tasks)
-        });
+async function getTasks() {
+    
+    try {
+        const response = await fetch('http://127.0.0.1/Todo-php-app/backend/apis/list-all.php')
+        const todos = await response.json();
+        renderTasks(todos)
+    } catch(error) {
+        console.error("ERROR GETTING TODO", error);
+    }
+
 }
 
-getTasks();
+function renderTasks(todo) {
+    const todos = todo.data
+    const todoContainer = document.querySelector('.todo__container');
+  
+    todos.forEach(todo => {
+        const todoElement = `
+            <div  class="todo">
+                <div class="todo__title">
+                    <div class="todo__title__item">
+                        <input value="true" type="radio" name="completed" id="completed">
+                        <p class="title">${todo.title}</p>
+                    </div>
+                    <ion-icon name="close-circle"></ion-icon>
+                </div>
+                <p class="description">${todo.description}</p>
+            </div>
+        `
+        todoContainer.innerHTML += todoElement;
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', getTasks);
+
+
